84e56c76836f039dbae207552fde20d8c6929488
martindev446 <martin.dev.446@gmail.com>
2022-03-08 13:30:16 -0500

    conan and tests setup

--- conanfile.py
+++ conanfile.py
@@ -1,1 +1,18 @@
-# note for mike to implement later
+from conans import ConanFile, CMake
+
+class PyRho(ConanFile):
+    name = "pyrho"
+    version = "2.0.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = (
+        "gtest/1.11.0",
+        "qt/6.2.3",
+    )
+    generators = "cmake", "cmake_find_package"
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
